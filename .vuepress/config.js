1d7c9de3810ab61cca474f0ff746c5e10eb1af49
unknown <ylyangliu1@gmail.com>
2021-07-15 15:23:10 -0700

    first small tweaks, learning vuepress

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,101 +1,97 @@
 module.exports = {
-  "title": "yangblog",
-  "description": "A blog for Yang.",
-  "dest": "public",
-  "head": [
+  title: "Yang Liu",
+  description: "A blog for Yang.",
+  dest: "public",
+  head: [
     [
       "link",
       {
-        "rel": "icon",
-        "href": "/favicon.ico"
-      }
+        rel: "icon",
+        href: "/favicon.ico",
+      },
     ],
     [
       "meta",
       {
-        "name": "viewport",
-        "content": "width=device-width,initial-scale=1,user-scalable=no"
-      }
-    ]
+        name: "viewport",
+        content: "width=device-width,initial-scale=1,user-scalable=no",
+      },
+    ],
   ],
-  "theme": "reco",
-  "themeConfig": {
-    "nav": [
+  theme: "reco",
+  themeConfig: {
+    nav: [
       {
-        "text": "Home",
-        "link": "/",
-        "icon": "reco-home"
+        text: "Home",
+        link: "/",
+        icon: "reco-home",
       },
       {
-        "text": "TimeLine",
-        "link": "/timeline/",
-        "icon": "reco-date"
+        text: "TimeLine",
+        link: "/timeline/",
+        icon: "reco-date",
       },
       {
-        "text": "Docs",
-        "icon": "reco-message",
-        "items": [
+        text: "Docs",
+        icon: "reco-message",
+        items: [
           {
-            "text": "vuepress-reco",
-            "link": "/docs/theme-reco/"
-          }
-        ]
+            text: "vuepress-reco",
+            link: "/docs/theme-reco/",
+          },
+        ],
       },
       {
-        "text": "Contact",
-        "icon": "reco-message",
-        "items": [
+        text: "Contact",
+        icon: "reco-message",
+        items: [
           {
-            "text": "GitHub",
-            "link": "https://github.com/recoluan",
-            "icon": "reco-github"
-          }
-        ]
-      }
-    ],
-    "sidebar": {
-      "/docs/theme-reco/": [
-        "",
-        "theme",
-        "plugin",
-        "api"
-      ]
-    },
-    "type": "blog",
-    "blogConfig": {
-      "category": {
-        "location": 2,
-        "text": "Category"
+            text: "GitHub",
+            link: "https://github.com/recoluan",
+            icon: "reco-github",
+          },
+        ],
       },
-      "tag": {
-        "location": 3,
-        "text": "Tag"
-      }
-    },
-    "friendLink": [
-      {
-        "title": "午后南杂",
-        "desc": "Enjoy when you can, and endure when you must.",
-        "email": "1156743527@qq.com",
-        "link": "https://www.recoluan.com"
-      },
-      {
-        "title": "vuepress-theme-reco",
-        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
-        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-        "link": "https://vuepress-theme-reco.recoluan.com"
-      }
     ],
-    "logo": "/logo.png",
-    "search": true,
-    "searchMaxSuggestions": 10,
-    "lastUpdated": "Last Updated",
-    "author": "Yang Liu",
-    "authorAvatar": "/avatar.png",
-    "record": "xxxx",
-    "startYear": "2017"
+    sidebar: {
+      "/docs/theme-reco/": ["", "theme", "plugin", "api"],
+    },
+    type: "blog",
+    blogConfig: {
+      category: {
+        location: 2,
+        text: "Category",
+      },
+      tag: {
+        location: 3,
+        text: "Tag",
+      },
+    },
+    // friendLink: [
+    //   {
+    //     title: "午后南杂",
+    //     desc: "Enjoy when you can, and endure when you must.",
+    //     email: "1156743527@qq.com",
+    //     link: "https://www.recoluan.com",
+    //   },
+    //   {
+    //     title: "Welcome to Yang's Blog",
+    //     desc: "Mobile and Web Developer",
+    //     avatar:
+    //       "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+    //     link: "https://vuepress-theme-reco.recoluan.com",
+    //   },
+    // ],
+    // logo: "/logo.png",
+    search: true,
+    searchMaxSuggestions: 10,
+    lastUpdated: "Last Updated",
+    author: "Yang",
+    authorAvatar: "/avatar.jpg",
+    record: "",
+    startYear: "2021",
   },
-  "markdown": {
-    "lineNumbers": true
-  }
-}
+  markdown: {
+    lineNumbers: true,
+  },
+};
